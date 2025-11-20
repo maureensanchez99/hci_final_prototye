685a44aa9940ab602adcb6e3b8243f39d94d4bbd
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-11-20 16:13:49 -0600

    adding some styling

--- lib/pages/medication.dart
+++ lib/pages/medication.dart
@@ -8,14 +8,30 @@ class ViewMedicationLogPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: const Center(
-        child: Text(
-          "View Medication Log",
-          style: TextStyle(
-            fontFamily: 'Merienda',
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            color: Colors.green,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 40),
+            Text(
+              "View Medication Log",
+              style: TextStyle(
+                fontFamily: 'Merienda',
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "Current medications will be displayed here.",
+              style: TextStyle(
+                fontFamily: 'Merienda',
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            )
+          ],
         ),
       ),
     );
